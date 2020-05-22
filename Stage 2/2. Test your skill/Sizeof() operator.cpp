c83043595c3657db47849f175000e60448f46d71
Shreyansh Gupta <54203870+shreyansh225@users.noreply.github.com>
2020-05-22 22:38:11 +0530

    Create Sizeof() operator.cpp

---
+++ Stage 2/2. Test your skill/Sizeof() operator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  //char a;
+   //int b;
+  //float c;
+  //double d;
+  std::cout<<sizeof(char)<<"\n";
+   std::cout<<sizeof(int)<<"\n";
+   std::cout<<sizeof(float)<<"\n";
+   std::cout<<sizeof(double)<<"\n";
+  
+}
