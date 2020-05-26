e659251d97dae36457e10a9b7162e1c806963c0a
Shreyansh Gupta <54203870+shreyansh225@users.noreply.github.com>
2020-05-26 16:30:13 +0530

    Update Digit root of a given number.cpp

--- Stage 4/2. Test your skill/Digit root of a given number.cpp
+++ Stage 4/2. Test your skill/Digit root of a given number.cpp
@@ -20,3 +20,25 @@ int main()
   std::cin>>n;
   std::cout<<dig(n,sum);
 }
+
+/*
+#include<iostream>
+int dig(int n,int s)
+{
+  if(s>9 || n>0)
+  {
+    if(n%10>0)
+    {
+      s=s+n%10;
+      return dig(n/10,s);
+    }
+    else
+      return dig(s,0);  
+  }
+}
+int main()
+{
+  int n,s=0;
+  std::cin>>n;
+  std::cout<<dig(n,s);
+}*/
