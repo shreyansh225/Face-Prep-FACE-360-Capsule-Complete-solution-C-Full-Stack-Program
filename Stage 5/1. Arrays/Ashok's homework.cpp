f5ac983e9c9b9008982ba67a613e88d5b442b216
Shreyansh Gupta <54203870+shreyansh225@users.noreply.github.com>
2020-05-28 12:42:19 +0530

    Create Ashok's homework.cpp

---
+++ Stage 5/1. Arrays/Ashok's homework.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+int main()
+{
+    int r, c;
+    std::cin>>r>>c;
+    int a[r][c],b[r][c];
+   for(int i = 0; i < r; i++)
+   {
+        for(int j = 0; j < c; j++)
+            std::cin>>a[i][j];
+        }
+  for(int i = 0; i < r; i++)
+  {
+        for(int j = 0; j < c; j++)
+            std::cin>>b[i][j];
+        }
+    for(int i = 0; i < r; i++)
+    {
+        for(int j = 0; j < c; j++)
+            std::cout<<a[i][j]+b[i][j]<<" ";     
+        std::cout<<"\n";
+    }
+}
