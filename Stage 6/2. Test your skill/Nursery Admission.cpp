a159f85d32dc86d3300ef5d389376076b9940ef7
Shreyansh Gupta <54203870+shreyansh225@users.noreply.github.com>
2020-05-30 11:11:31 +0530

    Create Nursery Admission.cpp

---
+++ Stage 6/2. Test your skill/Nursery Admission.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main()
+{
+  char str[50];
+  cin.getline(str,50);
+    cout<<"The number of letters in the name is "<<strlen(str);
+}
